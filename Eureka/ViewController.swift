cd50aad2b68bf978180e9932db291acc5a8ae9f3
Luke Mueller <lukemueller@protonmail.com>
2017-10-13 18:05:33 +0900

    Commit initially

---
+++ Eureka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eureka
+//
+//  Created by Luke Mueller on 13.10.17.
+//  Copyright © 2017 Jugend hackt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
